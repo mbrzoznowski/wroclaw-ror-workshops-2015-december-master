a9a3b77f6234d8fbea2aaf8b180c5e408a109843
Michal Brzoznowki <michal.brzoznowski@gmail.com>
2017-06-27 00:25:50 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :students do
     get :subjects
   end
+  resources :visitors, :teachers, :report_subjects
+  resources :reports do
+    get :subjects
+  end
+  root 'visitors#index'
 end
