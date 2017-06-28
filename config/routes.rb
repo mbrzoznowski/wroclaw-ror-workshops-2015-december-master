14337e61af4dbc0f4bceeb24f1ee6deaffb1ea1e
Michal Brzoznowki <michal.brzoznowski@gmail.com>
2017-06-29 01:40:38 +0200

    Fixed and added all necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :students, :teachers do
     get :subjects
   end
-  resources :visitors, :teachers, :report_subjects
-  resources :reports do
-    get :subjects
+  resources :visitors, :teachers, :report_subjects, :reports
+  namespace :reports do
+    resources :subjects
   end
+  get 'reports/subjects', to: 'reports#subjects'
   root 'visitors#index'
 end
