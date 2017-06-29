9a257aac772d3ac76d0ee3da0676e1406e562ead
Michal Brzoznowki <michal.brzoznowski@gmail.com>
2017-06-29 09:41:39 +0200

    Nothing changed

--- app/models/subject_item.rb
+++ app/models/subject_item.rb
@@ -1,8 +1,8 @@
 class SubjectItem < ActiveRecord::Base
   belongs_to :teacher
 
-  has_many :participations, dependent: :destroy
   has_many :students, through: :participations
+  has_many :participations, dependent: :destroy
   has_many :subject_item_notes
 
   validates :title, presence: true
